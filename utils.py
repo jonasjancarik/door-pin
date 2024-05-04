5bb169c5294e67275f6b834c7a87e410f5878a76
Jonas Jancarik <2459191+jonasjancarik@users.noreply.github.com>
2024-05-04 22:05:37 +0200

    Refactor RELAY_ACTIVATION_TIME in utils.py to reduce activation time

--- utils.py
+++ utils.py
@@ -3,12 +3,13 @@ import time
 
 # config
 RELAY_PIN = 18
-RELAY_ACTIVATION_TIME = 5  # seconds
+RELAY_ACTIVATION_TIME = 0.5  # seconds
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 GPIO.output(RELAY_PIN, GPIO.LOW)
+
 
 def unlock_door():
     GPIO.output(RELAY_PIN, GPIO.HIGH)
